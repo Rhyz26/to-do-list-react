fa60b5fb065cc3b298541ab2e7983a1d9834eb1f
Raymond <njagala.raymond@gmail.com>
2024-04-10 01:04:27 +0300

    styled the buttons

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -4,7 +4,7 @@ function Todo(){
   return (
     <div>
 <h1 className='text-center text-violet-700 mt-10 font-bold text-4xl '>TODO LIST</h1>
-        <div className='flex gap-52 pt-6  mb-6 justify-center'>
+        <div className='flex gap-52 pt-6  mb-6 mt-6 justify-center'>
 
         <button className="text-indigo-50 rounded-lg p-2 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">Add task</button>
         
@@ -13,7 +13,7 @@ function Todo(){
         
         <div className="flex justify-center">
 
-        <input placeholder='Enter a task' className='w-96 p-3'></input>
+        <input type="text" placeholder='Enter a task e.g Coding' className='w-96 p-3 border-8'></input>
         </div>
     </div>
   )
